189b5ed81b8417ba5d0c3d9add445bc93bf35a4c
naked123 <leonard9447@hotmail.com>
2020-05-06 23:46:15 +0700

    Create keyring.sh

---
+++ keyring.sh
@@ -0,0 +1,3 @@
+apt-get install --yes gnupg
+wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key
+apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc"
